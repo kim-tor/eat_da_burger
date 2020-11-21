b53d718d624b9d36ad39b165c6f628ae5f4ea8c6
Kim Torres <kimberley.5118@gmail.com>
2020-11-21 10:18:01 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- Drops the programming_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Created the DB "burgers_db" --
+CREATE DATABASE burgers_db;
+
+-- USE THE DB burgers_db for all the rest of the script --
+USE burgers_db;
+
+-- Created the table "burgers" --
+CREATE TABLE burgers (
+    
+)
