3a600bd5fd09fecb64276ca9532cca8e3e4dc5eb
Kim Torres <kimberley.5118@gmail.com>
2020-11-25 19:56:51 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3000,
@@ -8,10 +12,8 @@ var connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: "burgers_db"
 });
+};
 
-connection.connect(function(err){
-    if (err) throw err;
-    
-});
+connection.connect();
 
 module.exports = connection;
